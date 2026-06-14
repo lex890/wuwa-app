d4a395c7a3ced3ab6a1925faff53ae6bc9a538fc
Lexter Hiteroza <ashketchopstick@gmail.com>
2026-06-14 22:49:52 +0800

    adjusted Router Hook path

--- src/pages/Admin/Home/Home.jsx
+++ src/pages/Admin/Home/Home.jsx
@@ -3,10 +3,8 @@ import './Home.scss'
 function Home({ data }) {
   console.log(data)
   return(
-    <>
-      <div className="main-wrapper">
-        <h1>Welcome, Admin!</h1>
-      </div>
+    <>      
+      <h1>Welcome, Admin!</h1>
     </>
   )
 }
